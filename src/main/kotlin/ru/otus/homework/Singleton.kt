aed0f24c3e021d8bcaf9374b35ddf44027fda904
LexAndro3723 <glebkabackov9@gmail.com>
2023-05-12 12:53:37 +0300

    HomeWork_02

---
+++ src/main/kotlin/ru/otus/homework/Singleton.kt
@@ -0,0 +1,15 @@
+package ru.otus.homework
+
+class Singleton private constructor() {
+
+    companion object {
+        val instance:Singleton by lazy {
+            Singleton()
+        }
+    }
+
+}
+
+fun main() {
+    val instance = Singleton.instance
+}
