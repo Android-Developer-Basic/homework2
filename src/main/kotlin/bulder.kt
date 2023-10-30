1b118a327bec9494c6026ef335fc9017746d294e
viktoriurkov <victor.iurkov@gmail.com>
2023-10-30 21:35:06 +0300

    [HM-2] builder impl

---
+++ src/main/kotlin/bulder.kt
@@ -0,0 +1,3 @@
+class Human(var name: String = "John", var lastName: String = "Dow", var age: Int = 18)
+
+// Если использовать билдер как конфиг объекта с дефолт значениями - то вариант более удобный
